8b3746dd358ef796a5cdac49a846568914bf753e
junior-skater <cruzpinajunior123@gmail.com>
2023-02-22 16:43:50 -0400

    Co-authored-by: pxdeoleo <pxdeoleo@users.noreply.github.com>

--- fmc_module/functions.py
+++ fmc_module/functions.py
@@ -1,28 +1,75 @@
-import fmcapi
-from fmcapi.api_objects.object_services import *
+from fmcapi import *
+# from fmcapi.api_objects.object_services import *
 from keys import *
 
 
 
-def fmc_instance():
-	 return fmcapi.fmc.FMC(host=host, username=username, password=password, autodeploy=False)
+class SiteToSite():
 
-def create_net_object(fmc, name, value):
-	net = fmcapi.Networks(fmc= fmc, name= name, value= value)		
-	net.post()
-	
+	def __init__(self) -> None:
+		pass
 
-def create_host_object(fmc, name, value):
-	host = Hosts(fmc, name=name, value=value)
-	host.post()
+	# Create network objects
+	def create_net_object(name, value):
+		with FMC(host=host, username=username, password=password, autodeploy=False, 
+		) as fmc:
+			net = Networks(fmc=fmc, name= name, value= value)		
+			net.post()
+		
+	#  Create hosts objects
+	def create_host_object(name, value):
+		with FMC(host=host, username=username, password=password, autodeploy=False, 
+		) as fmc:
 
-def create_ike_policy(fmc, name,encryption, hash, DH):
-	'''ike_options = {
-        "name": name,
-        "encryption": encryption,
-        "hash": hash,
-        "diffieHellmanGroup": DH,
-        "authenticationMethod": "Preshared Key",
-        "lifetimeInSeconds": "86400"}'''
-	ike = IKEv1Policies(fmc=fmc, name=name, encryption=encryption, hash=hash,diffieHellmanGroup=DH, authenticationMethod="Preshared Key", lifetimeInSeconds=86400)
-	ike.post()
+			myhost = Hosts(fmc=fmc, name=name, value=value)
+			myhost.post()
+
+	#  Create ike version 1 policy
+	def create_ikev1_policy(name,encryption, hash, DH):
+		with FMC(host=host, username=username, password=password, autodeploy=False, 
+		) as fmc:
+				ike = IKEv1Policies(fmc=fmc, name=name
+				,encryption=encryption,
+				hash=hash,diffieHellmanGroup=DH, 
+				authenticationMethod="Preshared Key", 
+				lifetimeInSeconds=86400,
+				priority=20)
+				ike.post()
+
+	#  Create ike version 2 policy
+	def create_ikev2_policy(name,
+			integrityAlgorithms,
+			prfIntegrityAlgorithms,
+			encryptionAlgorithms,
+			diffieHellmanGroups,):
+		with FMC(host=host, username=username,
+		password=password,autodeploy=False
+		) as fmc:
+			ike = IKEv2Policies(fmc=fmc, name= name,
+				integrityAlgorithms= integrityAlgorithms,
+				prfIntegrityAlgorithms= prfIntegrityAlgorithms,
+				encryptionAlgorithms= encryptionAlgorithms,
+				diffieHellmanGroups= diffieHellmanGroups)
+			ike.post()
+
+	#  Create ike version 1 IPSEC proposal
+	def create_ikev1ipsec_proposal(name, espEncryption, espHash):
+		with FMC(host=host, username=username,
+		password=password,autodeploy=False
+		) as fmc:
+			ipsec = IKEv1IpsecProposals(fmc=fmc, name=name,
+			espEncryption=espEncryption, espHash= espHash)
+			ipsec.post()
+
+	#  Create ike version 2 IPSEC proposal
+	def create_ikev2ipsec_proposal(name, encryptionAlgorithms, integrityAlgorithms): 
+		with FMC(host=host, username=username,
+		password=password,autodeploy=False
+		) as fmc:
+			ipsec = IKEv2IpsecProposals(fmc=fmc, name=name,
+				encryptionAlgorithms=encryptionAlgorithms,
+				integrityAlgorithms=integrityAlgorithms)
+			ipsec.post()
+
+class SecPolicy():
+	pass
--- fmc_module/main.py
+++ fmc_module/main.py
@@ -1,9 +1,30 @@
-from functions import create_ike_policy, fmc_instance, create_host_object, create_net_object
+from functions import SiteToSite
+import csv 
+
+
+def readcsv(csv):
+    with open (csv, "r") as file:
+        params = csv.DictReader(file)
+    return params
+
+
+def vpn_creation():
+    vpn = SiteToSite()
+    vpn.create_net_object("nettest", "1.1.1.0/24")
+    vpn.create_host_object("hosttest", "1.1.1.7")
+    vpn.create_ikev1_policy("test123", "AES-128","SHA",14)
+    vpn.create_ikev2_policy("ikev2test","SHA","SHA","AES-256",14 )
+    vpn.create_ikev1ipsec_proposal ("ikev1ipsec", "AES-256","SHA")
+    vpn.create_ikev2ipsec_proposal("ikev2ipsec", "AES-256", "SHA-256")
+
 
 if __name__ == "__main__":
-    fmc = fmc_instance()
-    create_net_object(fmc, "nettest", "1.1.1.0/24")
-    create_host_object(fmc, "hosttest", "1.1.1.7")
-    create_ike_policy(fmc, "test123", "AES-128","SHA",14)
+    values = readcsv("file.csv")   
+    vpn_creation(values)
+
+
+
+
+
 
     
